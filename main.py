abf857731fd04bd5f6afcbc4a030fd43b56d509d
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2022-10-16 15:26:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 bot = Client(
     ":memory:",
     api_id= 17945796,
-    api_hash= "4a05481a5da2d66f801acffc4ca5ee4b"
+    api_hash= "4a05481a5da2d66f801acffc4ca5ee4b",
     bot_token= "5687118765:AAGd2l-MkzzPX2-43X96i8h1D1jRAML8g3Q"
 )
 
